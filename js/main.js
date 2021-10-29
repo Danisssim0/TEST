21b6fba9d2c81ca970232e216b64071afc7d2e67
Danisssim0 <daniil50592@yandex.ru>
2021-10-29 14:11:21 +0300

    АБОБА

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function onMouseOver(el){
+  el.style.color = "#A9A9A9";
+  //el.style.background = "white";
+}
+
+function onMouseOut(el){
+  el.style.color = "white";
+  //el.style.background = "#333";
+}
